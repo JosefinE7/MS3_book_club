b77876e054a44700e339bc02cc78ee07735b0bb1
JosefinE7 <fin.jos@hotmail.com>
2020-11-09 10:40:26 +0000

    Add page title for profile.html

--- run.py
+++ run.py
@@ -96,7 +96,7 @@ def profile(username):
     # grab the session user's username from db
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
-    return render_template("profile.html", username=username)
+    return render_template("profile.html", username=username, page_title="Profile")
 
 
 if __name__ == "__main__":
