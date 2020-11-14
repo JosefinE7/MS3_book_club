a8fc318b17ca99d1e68e208aa26f60a96431d9b6
JosefinE7 <fin.jos@hotmail.com>
2020-11-14 13:24:58 +0000

    Fix proper indentation in '/register' run.py

--- run.py
+++ run.py
@@ -36,27 +36,26 @@ def search():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "GET":
-        return render_template("register.html", page_title="Register")
+        return render_template("register.html")
 
-    else:
-        # Check if username already exists in db
-        existing_user = mongo.db.users.find_one(
-            {"username": request.form.get("username").lower()})
+    # Check if username already exists in db
+    existing_user = mongo.db.users.find_one(
+        {"username": request.form.get("username").lower()})
 
-        if existing_user:
-            flash("Username already exists!")
-            return redirect(url_for("register"))
+    if existing_user:
+        flash("Username already exists!")
+        return redirect(url_for("register"))
 
-        register = {
-            "username": request.form.get("username").lower(),
-            "password": generate_password_hash(request.form.get("password"))
-        }
-        mongo.db.users.insert_one(register)
+    register = {
+        "username": request.form.get("username").lower(),
+        "password": generate_password_hash(request.form.get("password"))
+    }
+    mongo.db.users.insert_one(register)
 
-        # put the new user into 'session' cookie
-        session["user"] = request.form.get("username").lower()
-        flash("Registration Successful!")
-        return redirect(url_for("profile", username=session["user"]))
+    # put the new user into 'session' cookie
+    session["user"] = request.form.get("username").lower()
+    flash("Registration Successful!")
+    return redirect(url_for("profile", username=session["user"]))
 
 
 @app.route("/login", methods=["GET", "POST"])
